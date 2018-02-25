b4f5d4bdb78123935fb2c931d206400465a3ae7b
Luis <lfr4704@gmail.com>
2018-02-24 23:43:35 -0800

    revised

--- js/script.js
+++ js/script.js
@@ -25,3 +25,30 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.rect(20, 20, 100, 100);
 ctx.stroke();
+
+// this is to add scrolling effect to anchors
+
+$(document).ready(function(){
+ 
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
